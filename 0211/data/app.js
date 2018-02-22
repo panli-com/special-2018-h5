ebe1f18d2bdfbddcac1778d89d09d3cf73519731
zanjs <root@zanjs.com>
2018-02-22 13:25:14 +0800

    今天好懒 :grinning:

--- 0211/data/app.js
+++ 0211/data/app.js
@@ -8,52 +8,47 @@ function FloorList(data) {
     fl6 = [],
     fl0 = [];
 
-  for (var i = 0; i < mlc; i++) {
+  for (var i = 0; i < mlc + 4; i++) {
     fl0.push(data[i]);
   }
-  for (var i = mlc; i < mlc * 2; i++) {
+  for (var i = mlc + 4; i < mlc * 2 + 4; i++) {
     fl1.push(data[i]);
   }
-  for (var i = mlc * 2; i < mlc * 3; i++) {
+  for (var i = mlc * 2 + 4; i < mlc * 3 + 4; i++) {
     fl2.push(data[i]);
   }
-  for (var i = mlc * 3; i < mlc * 4; i++) {
+  for (var i = mlc * 3 + 4; i < mlc * 4 + 4; i++) {
     fl3.push(data[i]);
   }
-  for (var i = mlc * 4; i < mlc * 5; i++) {
+  for (var i = mlc * 4 + 4; i < mlc * 5 + 4; i++) {
     fl4.push(data[i]);
   }
-  for (var i = mlc * 5; i < mlc * 6; i++) {
+  for (var i = mlc * 5 + 4; i < mlc * 6 + 4; i++) {
     fl5.push(data[i]);
   }
   // for (var i = mlc * 6; i < mlc * 7; i++) {
   //     fl6.push(data[i]);
   // }
 
-
-
-  FloorAll(fl0, 'cx', 1);
-  FloorAll(fl1, 'ts', 2);
-  FloorAll(fl2, 'rw', 3);
-  FloorAll(fl3, 'sh', 4);
-  FloorAll(fl4, 'sh', 5);
+  FloorAll(fl0, "cx", 1);
+  FloorAll(fl1, "ts", 2);
+  FloorAll(fl2, "rw", 3);
+  FloorAll(fl3, "sh", 4);
+  FloorAll(fl4, "sh", 5);
   // FloorAll(fl5, 'sh', 6);
   // FloorAll(fl6, 'sh', 7);
-
-
 }
 
 function FloorAll(data, imgHead, id) {
-
-  var str = '';
+  var str = "";
   for (var i = 0; i < data.length; i++) {
     var name = data[i].店铺名称,
       price = data[i].价格,
-      imgUrl = './build/css/img/list/' + data[i].图片名称,
+      imgUrl = "./build/css/img/list/" + data[i].图片名称,
       Url = data[i].商品导购,
       shopUrl = data[i].店铺链接;
 
-      str += `
+    str += `
       <li>
                     <div class="thumb">
                         <a href="${Url}"  target="_blank">
@@ -72,8 +67,9 @@ function FloorAll(data, imgHead, id) {
                         </a>
                     </div>
                 </li>
-      `
-
-  };
-  PD(".floor" + id).find("ul").html(str);
+      `;
+  }
+  PD(".floor" + id)
+    .find("ul")
+    .html(str);
 }
