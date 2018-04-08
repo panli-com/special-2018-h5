f1ab3315d5c1fced5e0663162cc658ae6ebf80a2
zanjs <suihuasheng@gmail.com>
2018-04-08 11:32:14 +0800

    add 四月烘焙节H5

---
+++ 0408/data/data.js
@@ -0,0 +1,83 @@
+var productData = 
+[
+  {
+    "店铺名称": "展艺旗舰店",
+    "店铺链接": "https://artexhibition.tmall.com/ "
+  },
+  {
+    "店铺名称": "乐趣烘焙DIY",
+    "店铺链接": "https://guxiaobei.taobao.com/"
+  },
+  {
+    "店铺名称": "乐回家烘焙",
+    "店铺链接": "https://shop57244217.taobao.com/"
+  },
+  {
+    "店铺名称": "比蛋糕店的更精致",
+    "店铺链接": "https://shop73031264.taobao.com/"
+  },
+  {
+    "店铺名称": "健康 是一种生活态度",
+    "店铺链接": "https://shop33254243.taobao.com/"
+  },
+  {
+    "店铺名称": "童趣世界 其乐无穷",
+    "店铺链接": "https://shop66995275.taobao.com/"
+  },
+  {
+    "店铺名称": "亘祖烘焙包装批发",
+    "店铺链接": "https://shop103501077.taobao.com/"
+  },
+  {
+    "店铺名称": "卓尔烘焙",
+    "店铺链接": "https://diy78.taobao.com/"
+  },
+  {
+    "店铺名称": "匠心造物 把爱装给你",
+    "店铺链接": "https://jiyoujia59900868.jiyoujia.com/"
+  },
+  {
+    "店铺名称": "暖心烘焙系列",
+    "店铺链接": "https://shop113507921.taobao.com/"
+  },
+  {
+    "店铺名称": "品质保障 用户服务",
+    "店铺链接": "https://shop136489641.taobao.com/"
+  },
+  {
+    "店铺名称": "彼岸食尚 ",
+    "店铺链接": "https://supertaste.jiyoujia.com/"
+  },
+  {
+    "店铺名称": "给TA一份小惊喜",
+    "店铺链接": "https://shop111474628.taobao.com/"
+  },
+  {
+    "店铺名称": "哥哥的烘焙屋",
+    "店铺链接": "https://shop114790429.taobao.com/"
+  },
+  {
+    "店铺名称": "温暖包邮 全场热卖中",
+    "店铺链接": "https://shop71998774.taobao.com/"
+  },
+  {
+    "店铺名称": "烘焙用品行家",
+    "店铺链接": "https://shop131291388.taobao.com/"
+  },
+  {
+    "店铺名称": "专注美食 乐享烘焙",
+    "店铺链接": "https://shop124460871.taobao.com/"
+  },
+  {
+    "店铺名称": "工厂零售店",
+    "店铺链接": "https://shop108517440.taobao.com/"
+  },
+  {
+    "店铺名称": "范特西烘焙DIY",
+    "店铺链接": "https://shop102296846.taobao.com/ "
+  },
+  {
+    "店铺名称": "乾能烘焙 激发潜能",
+    "店铺链接": "https://qiannengwx.tmall.com/"
+  }
+]
