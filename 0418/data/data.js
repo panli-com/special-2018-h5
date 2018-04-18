a34419893e6a8ab5f086ef3e088e23b6a8a6740d
zanjs <suihuasheng@gmail.com>
2018-04-18 13:27:13 +0800

    up 男神节

---
+++ 0418/data/data.js
@@ -0,0 +1,115 @@
+var productData = 
+[
+  {
+    "店铺名称": "年轻人的购物潮流中心",
+    "店铺链接": "https://yoho.tmall.com/"
+  },
+  {
+    "店铺名称": "大码潮牌男装",
+    "店铺链接": "https://rmaker.tmall.com/"
+  },
+  {
+    "店铺名称": "小尼力街头嘻哈潮流店",
+    "店铺链接": "https://shop36412463.taobao.com/"
+  },
+  {
+    "店铺名称": "SHOWHANDS梭哈原创品牌",
+    "店铺链接": "https://showhands.taobao.com/"
+  },
+  {
+    "店铺名称": "Realsport 美式街头潮牌总店",
+    "店铺链接": "https://realsport.taobao.com/"
+  },
+  {
+    "店铺名称": "阿吉拉姆品牌店",
+    "店铺链接": "https://ajram.taobao.com/"
+  },
+  {
+    "店铺名称": "INXXMAN香港潮流品牌",
+    "店铺链接": "https://shop113930498.taobao.com/"
+  },
+  {
+    "店铺名称": "SUPER ZOO STUDIO",
+    "店铺链接": "https://shizitong.taobao.com/"
+  },
+  {
+    "店铺名称": "YESMAN原创潮流男装",
+    "店铺链接": "https://yes-man.taobao.com/"
+  },
+  {
+    "店铺名称": "ARTS MAN STUDIO",
+    "店铺链接": "https://lp930428.taobao.com/"
+  },
+  {
+    "店铺名称": "潮叁 捍卫街头正品",
+    "店铺链接": "https://chaosan.taobao.com/"
+  },
+  {
+    "店铺名称": "RANDOMEVENT 线上商店",
+    "店铺链接": "https://rdet.taobao.com/"
+  },
+  {
+    "店铺名称": "一町魂原创男装工作室",
+    "店铺链接": "https://newgboy.taobao.com/"
+  },
+  {
+    "店铺名称": "威廉威定制 william look",
+    "店铺链接": "https://william.taobao.com/"
+  },
+  {
+    "店铺名称": "季野潮服 你型我帅",
+    "店铺链接": "https://gyee.taobao.com/"
+  },
+  {
+    "店铺名称": "原创自主品牌",
+    "店铺链接": "https://viviivii.taobao.com/"
+  },
+  {
+    "店铺名称": " hvelay潮牌男装",
+    "店铺链接": "https://hegz02.taobao.com/"
+  },
+  {
+    "店铺名称": "东京衣柜男装工作室",
+    "店铺链接": "https://boyandman.taobao.com/"
+  },
+  {
+    "店铺名称": "潮男时尚搭配",
+    "店铺链接": "https://shop188191461.taobao.com/"
+  },
+  {
+    "店铺名称": "主打日系复古男装",
+    "店铺链接": "https://raul22.taobao.com/"
+  },
+  {
+    "店铺名称": "子俊男装",
+    "店铺链接": "https://ladypapa.taobao.com/"
+  },
+  {
+    "店铺名称": "MIXSEVEN原创设计师店",
+    "店铺链接": "https://mixseven.taobao.com/"
+  },
+  {
+    "店铺名称": "国潮独立设计嘻哈风",
+    "店铺链接": "https://mengmate.taobao.com/"
+  },
+  {
+    "店铺名称": "大儿童古古 改良款中国风男装",
+    "店铺链接": "https://in-mask.taobao.com/"
+  },
+  {
+    "店铺名称": "夏季流行男装",
+    "店铺链接": "https://shop148920545.taobao.com/"
+  },
+  {
+    "店铺名称": "极简男装专营店",
+    "店铺链接": "https://jjnz.tmall.com/"
+  },
+  {
+    "店铺名称": "商务新风尚",
+    "店铺链接": "https://taizihui.tmall.com/"
+  },
+  {
+    "店铺名称": "迪尔马奇品牌自营店",
+    "店铺链接": "https://junwei.taobao.com/"
+  }
+]
