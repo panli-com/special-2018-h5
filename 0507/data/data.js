5fd6597b4c1ad1a6bffecbac37ce278a6bcf5138
zanjs <suihuasheng@gmail.com>
2018-05-07 13:47:43 +0800

    up 母亲节

---
+++ 0507/data/data.js
@@ -0,0 +1,43 @@
+var productData = 
+[
+  {
+    "店铺名称": "年轻人的购物潮流中心",
+    "店铺链接": "https://www.taobao.com/markets/xie/wystyle"
+  },
+  {
+    "店铺名称": "大码潮牌男装",
+    "店铺链接": "https://pages.tmall.com/wow/chaoshi/act/lsqxg-1?spm=a3204.7084713.1997320513.d4.YvLmGB"
+  },
+  {
+    "店铺名称": "小尼力街头嘻哈潮流店",
+    "店铺链接": "https://mst.vip.com/ILrqooO7uYTGdbINEPkvIg.php?client=vipcms&wapid=mst_4911183&adidx=2"
+  },
+  {
+    "店铺名称": "SHOWHANDS梭哈原创品牌",
+    "店铺链接": "http://act.mogujie.com/uzt/pc/changxiao"
+  },
+  {
+    "店铺名称": "Realsport 美式街头潮牌总店",
+    "店铺链接": "https://sale.jd.com/act/lkHZu15bCpm4Xx.html?cpdad=1DLSUE"
+  },
+  {
+    "店铺名称": "阿吉拉姆品牌店",
+    "店铺链接": "https://sale.yhd.com/act/1shg8AJCcjYmn.html"
+  },
+  {
+    "店铺名称": "INXXMAN香港潮流品牌",
+    "店铺链接": "http://book.dangdang.com/20180427_x0x4"
+  },
+  {
+    "店铺名称": "SUPER ZOO STUDIO",
+    "店铺链接": "https://www.amazon.cn/b?_encoding=UTF8&node=1977993071"
+  },
+  {
+    "店铺名称": "YESMAN原创潮流男装",
+    "店铺链接": "http://topic.moonbasa.com/article/article_show/8174?cn=84848&type=1&adsiteid=10000007"
+  },
+  {
+    "店铺名称": "ARTS MAN STUDIO",
+    "店铺链接": "https://www.m18.com/shop/inman?banner_no=185042"
+  },
+]
